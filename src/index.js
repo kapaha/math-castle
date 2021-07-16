9f6608b8eba149dc2f42c2a676558474b480bfd3
Kai Paterson - Hall <kaizwebdesign@gmail.com>
2021-07-16 17:46:37 +0100

    Add game loop
    Add a game loop, update and draw functions. The game loop fn will be
    called before the browser performs the next repaint using
    requestAnimationFrame(). This simulates having FPS.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './styles/styles.css';
+
+function update() {
+    // update game objects
+}
+
+function draw() {
+    // draw game objects
+}
+
+function gameLoop() {
+    update();
+    draw();
+    requestAnimationFrame(gameLoop);
+}
+
+// start the game loop
+requestAnimationFrame(gameLoop);
