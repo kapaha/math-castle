7ff9bcbc94a4eecc1db7c7f2d1bc78b80f714c1f
Kai Paterson - Hall <kaizwebdesign@gmail.com>
2021-07-17 17:48:42 +0100

    Setup Game constructor

--- src/game.js
+++ src/game.js
@@ -1,3 +1,12 @@
-class Game {}
+import gameBoard from './gameBoard';
+import castle from './castle';
+
+class Game {
+    constructor() {
+        this.gameBoard = gameBoard;
+        this.castle = castle;
+        this.enemies = [];
+    }
+}
 
 export default Game;
