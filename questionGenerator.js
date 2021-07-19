d7f174674c19302a48de89b01b5d9b377ca9db30
Oliver Coldwell <olivercoldwell@gmail.com>
2021-07-19 23:23:13 +0100

    added function that takes a question and returns the answer, this only works for two numbers at the moment

--- questionGenerator.js
+++ questionGenerator.js
@@ -1,34 +1,55 @@
 const operatorDifficulty = {
-  'easy': ['+', '-'],
-  'medium': [ '+', '-', '*'],
-  'hard': ['+', '-', '*'],
-  'insane': ['+', '-', '*', '/']
-}
+    easy: ['+', '-'],
+    medium: ['+', '-', '*'],
+    hard: ['+', '-', '*'],
+    insane: ['+', '-', '*', '/'],
+};
 
 const wholeNumberGenerator = (number) => Math.floor(Math.random() * number);
 
 const generateDifficulty = (difficulty) => {
     switch (difficulty) {
-      case 'easy':
-        return 5;
-      case 'medium':
-        return 10;
-      case 'hard':
-        return 30;
-      case 'insane':
-        return 100;
-      default:
-        return 5;
+        case 'easy':
+            return 5;
+        case 'medium':
+            return 10;
+        case 'hard':
+            return 30;
+        case 'insane':
+            return 100;
+        default:
+            return 5;
     }
-}
-const indexOperatorGenerator = difficulty => Math.floor(Math.random() * operatorDifficulty[difficulty].length);
+};
+const indexOperatorGenerator = (difficulty) =>
+    wholeNumberGenerator(operatorDifficulty[difficulty].length);
 
-const operatorSelector = difficulty => operatorDifficulty[difficulty][indexOperatorGenerator(difficulty)];
+const operatorSelector = (difficulty) =>
+    operatorDifficulty[difficulty][indexOperatorGenerator(difficulty)];
 
-const questionNumberGenerator = (difficulty) => wholeNumberGenerator(generateDifficulty(difficulty))
+const questionNumberGenerator = (difficulty) =>
+    wholeNumberGenerator(generateDifficulty(difficulty));
 
 const questionGenerator = (difficulty) => {
-  const operator = operatorSelector(difficulty);
-  const question =  `${questionNumberGenerator()} ${operator} ${questionNumberGenerator()}`;
-  console.log(question);
-}
+    const operator = operatorSelector(difficulty);
+    const question = `${questionNumberGenerator()} ${operator} ${questionNumberGenerator()}`;
+    return question;
+};
+
+const answerQuestion = (question) => {
+    const questionArr = question.split(' ');
+    const num1 = Number(questionArr[0]);
+    const operator = questionArr[1];
+    const num2 = Number(questionArr[2]);
+    let answer;
+    if (operator === '+') {
+        answer = num1 + num2;
+    } else if (operator === '-') {
+        answer = num1 - num2;
+    } else if (operator === '*') {
+        answer = num1 * num2;
+    } else {
+        answer = num1 / num2;
+    }
+    return answer;
+};
