e74986895704a9b061f2daa241fa0c17c3e92519
Kai Paterson - Hall <kaizwebdesign@gmail.com>
2021-07-20 08:35:00 +0100

    Move JS modules to own directory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles/styles.css';
-import Game from './game';
-import Engine from './engine';
+import Game from './modules/game';
+import Engine from './modules/engine';
 
 const game = new Game();
 const engine = new Engine(game.update, game.draw);
